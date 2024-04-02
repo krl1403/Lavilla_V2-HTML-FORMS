c9cdcace428501ed6a203b5710d72992d0589b1d
krl1403 <kennethlavilla1403@gmail.com>
2024-04-02 10:34:02 +0800

    added background color and shadow on buttons

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 function toggleDropdown(button) {
-    var types = button.nextElementSibling;
-    if (types.style.display === "block") {
-      types.style.display = "none";
-    } else {
-      types.style.display = "block";
-      var items = types.getElementsByClassName("types-item");
-      for (var i = 0; i < items.length; i++) {
-        items[i].addEventListener("click", function() {
-          types.style.display = "none";
-          button.querySelector("span").textContent = this.textContent;
-        });
-      }
+  var types = button.nextElementSibling;
+
+  if (types.style.display === "block") 
+    types.style.display = "none";
+  else {
+    types.style.display = "block";
+
+    var items = types.getElementsByClassName("types-item");
+
+    for (var i = 0; i < items.length; i++) {
+      items[i].addEventListener("click", function() {
+        types.style.display = "none";
+        button.querySelector("span").textContent = this.textContent;
+      });
     }
   }
+}
